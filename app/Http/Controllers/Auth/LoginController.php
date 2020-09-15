775fe24fa8845239fe47c92942b11b83008144d5
JerllonDino <jerllon03@gmail.com>
2020-09-15 23:39:28 +0800

    login-fix-heroku

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/admin/documents';
 
     /**
      * Create a new controller instance.
