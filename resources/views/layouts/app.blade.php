8f1dbe83c52a001725ff11b16ff3b7166ad9f790
JerllonDino <jerllon03@gmail.com>
2021-04-29 16:57:21 +0800

    add meta tag
    Signed-off-by: JerllonDino <jerllon03@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,81 +1,92 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Bagulin</title>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        @yield('styles')
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/solid.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/brands.min.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-        <link rel="shortcut icon" href="{{ asset('img/logo-2.png') }}"/>
+<head>
+  <title>Municipality of Bagulin</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Municipality of Bagulin Government Website">
 
-        <style>
-          .p-5{
-              background: url('img/samplebg.png');
-              background-size: cover;
-              text-shadow: 0px 0px 8px rgba(0,0,0,0.9);
-            }
-            #intro{
-              width: 100%;
-              background-color: rgba(0,0,0,0.1);
-              box-shadow: 0px 5px 10px rgba(0,0,0,0.3);
-              padding: 20px;
-              margin: none;
-              border-collapse: collapse;
-            }
-            .carousel-item {
-              width: 100%;
-            }
-            .m-frame{
-              /* margin-top: 50%; */
-              border:5px solid #B53A3A;
-              box-shadow: 0px 5px 10px rgba(0,0,0,0.3);
-              background-color: rgba(0,0,0,0.09);
-            }
-            .ch{
-              overflow: hidden;
-            }
-            a .ch-click{
-              opacity: 0.9;
-              border-radius: 3%;
-              filter: alpha(opacity=50);
-              transition: transform 0.2s;
-            }
-          a:hover .ch-click{
-              transform: scale(1.05);
-              transform-origin: 50% 50%;
-              opacity: 1;
-              filter: alpha(opacity=50);
-              box-shadow: -3px 3px 10px -1px black;
-            }
-            .italic{
-              font-style: italic;
-              font-family: Monospace;
-            }
+  @yield('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/solid.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/brands.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+  <link rel="shortcut icon" href="{{ asset('img/logo-2.png') }}" />
+
+  <style>
+    .p-5 {
+      background: url('img/samplebg.png');
+      background-size: cover;
+      text-shadow: 0px 0px 8px rgba(0, 0, 0, 0.9);
+    }
+
+    #intro {
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.1);
+      box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+      padding: 20px;
+      margin: none;
+      border-collapse: collapse;
+    }
+
+    .carousel-item {
+      width: 100%;
+    }
+
+    .m-frame {
+      /* margin-top: 50%; */
+      border: 5px solid #B53A3A;
+      box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+      background-color: rgba(0, 0, 0, 0.09);
+    }
+
+    .ch {
+      overflow: hidden;
+    }
+
+    a .ch-click {
+      opacity: 0.9;
+      border-radius: 3%;
+      filter: alpha(opacity=50);
+      transition: transform 0.2s;
+    }
+
+    a:hover .ch-click {
+      transform: scale(1.05);
+      transform-origin: 50% 50%;
+      opacity: 1;
+      filter: alpha(opacity=50);
+      box-shadow: -3px 3px 10px -1px black;
+    }
+
+    .italic {
+      font-style: italic;
+      font-family: Monospace;
+    }
 
 
-            .par{
-              margin-top: -15px;
-            }
-            h4{
-              color: #B53A3A;
-            }
-        </style>
-    </head>
-    <body class="bg-body">
-        @include('layouts/incs/navbar')
-        @include('layouts/incs/banner')
+    .par {
+      margin-top: -15px;
+    }
 
-        @yield('content')
+    h4 {
+      color: #B53A3A;
+    }
+  </style>
+</head>
 
-        @include('layouts/incs/footers')
-        <script>
-          function setCookie(name,value,days) {
+<body class="bg-body">
+  @include('layouts/incs/navbar')
+  @include('layouts/incs/banner')
+
+  @yield('content')
+
+  @include('layouts/incs/footers')
+  <script>
+    function setCookie(name,value,days) {
             var expires = "";
             if (days) {
                 var date = new Date();
@@ -108,7 +119,8 @@
             });
             setCookie('page_viewed', 'TRUE', 1);
           }
-        </script>
-        @yield('scripts')
-    </body>
+  </script>
+  @yield('scripts')
+</body>
+
 </html>
